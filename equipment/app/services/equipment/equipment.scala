c7df1421ed46039728602bcf68a7accc250404ad
Andy Hull <ahull@sungevity.com>
2015-06-18 19:40:12 -0700

    Added additional inverter properties

--- equipment/app/services/equipment/equipment.scala
+++ equipment/app/services/equipment/equipment.scala
@@ -6,6 +6,6 @@ sealed trait Equipment {
 	def description: Option[String]
 }
 
-case class Inverter (id: Int, model: String, description: Option[String], efficiency: Double) extends Equipment
+case class Inverter (id: Int, model: String, description: Option[String], efficiency: Double, outputVoltage: Option[Double], isThreePhase: Option[Boolean]) extends Equipment
 
-case class Module (id: Int, model: String, description: Option[String]) extends Equipment
+case class Module (id: Int, model: String, description: Option[String], kwStc: Double, kwPtc: Double, heightMm: Double, widthMm: Double) extends Equipment
--- equipment/app/services/equipment/slick/SlickBackedEquipmentService.scala
+++ equipment/app/services/equipment/slick/SlickBackedEquipmentService.scala
@@ -13,14 +13,24 @@ import services.equipment._
 class SlickBackedEquipmentService @Inject()(@NamedDatabase("equipment") dbConfigProvider: DatabaseConfigProvider) extends EquipmentService {
 	val dbConfig = dbConfigProvider.get[JdbcProfile]
 
-	class EquipmentTable(tag: Tag) extends Table[(Int, Int, String, Option[String], Option[Double])](tag, "equipment") {
+	class EquipmentTable(tag: Tag) extends Table[(Int, Int, String, Option[String], Option[Double], Option[Double], Option[Double], Option[Double], Option[Double], Option[Double], Option[Boolean])](tag, "equipment") {
 		def id = column[Int]("id", O.PrimaryKey) // This is the primary key column
 		def equipmentTypeId = column[Int]("equipment_type_id")
 		def model = column[String]("model")
 		def description = column[Option[String]]("description")
-		def inverterEfficiency = column[Option[Double]]("inverter_efficiency")
 
-		def * = (id, equipmentTypeId, model, description, inverterEfficiency)
+		/* module columns */
+		def panelKwStc = column[Option[Double]]("panel_kw_stc")
+        def panelKwPtc = column[Option[Double]]("panel_kw_ptc")
+        def panelHeightMm = column[Option[Double]]("panel_height_mm")
+        def panelWidthMm = column[Option[Double]]("panel_width_mm")
+
+        /* inverter columns */
+		def inverterEfficiency = column[Option[Double]]("inverter_efficiency")
+		def inverterOutputVoltage = column[Option[Double]]("inverter_output_voltage")
+        def inverterIsThreePhase = column[Option[Boolean]]("inverter_is_three_phase")
+
+		def * = (id, equipmentTypeId, model, description, panelKwStc, panelKwPtc, panelHeightMm, panelWidthMm, inverterEfficiency, inverterOutputVoltage, inverterIsThreePhase)
 	}
 	val equipment = TableQuery[EquipmentTable]
 
@@ -28,10 +38,14 @@ class SlickBackedEquipmentService @Inject()(@NamedDatabase("equipment") dbConfig
 	def getEquipment(equipmentId: Int): Future[Option[Equipment]] = {
   		val foundEquipment = dbConfig.db.run(equipment.filter(_.id === equipmentId).result)
   		
+  		//TODO join on equipment type and manufacturer
+
   		foundEquipment map { results => results.headOption map { 
-  			case (id, 1, model, description, Some(inverterEfficiency)) => Inverter(id, model, description, inverterEfficiency)
-  			case (id, 2, model, description, None) => Module(id, model, description)
-  			case _ => throw new Exception("Unable to map result")
+  			case (id, 1, model, description, None, None, None, None, Some(inverterEfficiency), inverterOutputVoltage, inverterIsThreePhase) => 
+  				Inverter(id, model, description, inverterEfficiency, inverterOutputVoltage, inverterIsThreePhase)
+  			case (id, 2, model, description, Some(panelKwStc), Some(panelKwPtc), Some(panelHeightMm), Some(panelWidthMm), None, None, None) => 
+  				Module(id, model, description, panelKwStc, panelKwPtc, panelHeightMm, panelWidthMm)
+  			case _ => throw new Exception("Unable to map result") //TODO better error handling
   		} }
 	}
 
